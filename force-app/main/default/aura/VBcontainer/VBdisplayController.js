26435edb6f3e038ce17ab99b2a28ab06d10e25dd
Matari Howard <howardmatari@gmail.com>
2022-01-15 17:29:42 -0500

    chnaged comment for soql query added

--- force-app/main/default/aura/VBcontainer/VBdisplayController.js
+++ force-app/main/default/aura/VBcontainer/VBdisplayController.js
@@ -1,5 +1,5 @@
 public class VBdisplayController {
-    //Below method will return list of makes
+    //Nested Soql query to get requried data from product2
      @AuraEnabled
      public static List<Make__c> getMake(){
          List<Make__c> makelist = [SELECT Vehicle_Make__c, Vehicle_Model__c, Year__c, (SELECT ID, Name FROM Customization_Catalogue_Entries__r), (SELECT ID, Name FROM Paint_Color_Catalogue_Entries__r), (SELECT ID, Name FROM Accessory_Catalogue_Entries__r) FROM Product2];
